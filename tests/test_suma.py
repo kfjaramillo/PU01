3d2dd2cdb8920808f588833fa6dd6cfb4e852ca2
kfjaramillo <kfjaramillo@puce.edu.ec.com>
2024-07-02 13:41:12 -0500

    commit7

--- tests/test_suma.py
+++ tests/test_suma.py
@@ -4,7 +4,7 @@ from Modsuma.suma import add # type: ignore # Importa la función add desde el m
 class TestAddFunction(unittest.TestCase): # definición de la clase de prueba
     def test_add(self): # método de prueba
         self.assertEqual(add(2, 3), 5) # Llama a la función add con los argumentos 2 y 3, y verifica que el resultado sea 5. Si el resultado no es 5, la prueba fallará.
-        self.assertEqual(add(-1, 1), 6)
+        self.assertEqual(add(-1, 1), 0)
         self.assertEqual(add(0, 0), 0)
 
 if __name__ == '__main__':  #  Esta línea verifica si el script se está ejecutando directamente (no importado como un módulo)
